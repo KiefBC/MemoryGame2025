423ddc7abbabf58d10fdd73d91cd7b59281d7200
KiefBC <kiefhay642@outlook.com>
2025-01-09 10:20:45 -0800

    Finished Lab 1

--- MemoryGame2025/ContentView.swift
+++ MemoryGame2025/ContentView.swift
@@ -8,12 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    let theSun = ["sun.max", "cloud.sun", "cloud", "cloud.rain"]
+    @State private var current = 0
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        HStack {
+            Button(action: {
+                current = (current - 1 + theSun.count) % theSun.count
+            }) {
+                Image(systemName: "arrowtriangle.left")
+                    .font(.system(size: 50))
+                    .foregroundColor(.blue)
+            }
+            
+            VStack {
+                // Sun... images
+                Image(systemName: theSun[current])
+                    .font(.system(size: 150))
+                    .foregroundColor(.green)
+                
+                Text("\(theSun[current])")
+                    .font(.system(size: 25))
+                    .foregroundColor(.green)
+            }
+                
+            
+            // Right Button
+            Button(action: {
+                current = (current - 1 + theSun.count) % theSun.count
+            }) {
+                Image(systemName: "arrowtriangle.right")
+                    .font(.system(size: 50))
+                    .foregroundColor(.blue)
+            }
+            
         }
         .padding()
     }
