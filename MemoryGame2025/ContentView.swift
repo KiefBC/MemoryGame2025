dde9e6297d7be587f295689dbfeea728cbb81ad1
KiefBC <kiefhay642@outlook.com>
2025-01-09 09:34:33 -0800

    Initial Commit

---
+++ MemoryGame2025/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MemoryGame2025
+//
+//  Created by IMD 224 on 2025-01-09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
